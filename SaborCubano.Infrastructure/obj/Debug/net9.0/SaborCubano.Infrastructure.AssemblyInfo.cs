0f3396aacc7eb1f86abf9d0e2e85963670cdb690
JohnyYen <126342433+JohnyYen@users.noreply.github.com>
2024-12-03 08:59:17 -0500

    no-change

--- SaborCubano.Infrastructure/obj/Debug/net9.0/SaborCubano.Infrastructure.AssemblyInfo.cs
+++ SaborCubano.Infrastructure/obj/Debug/net9.0/SaborCubano.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SaborCubano.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d500b676770f8f2c65b6af98edfe77b85feafb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27f9c8ae62eda3a58a8304a6280727856ea96f07")]
 [assembly: System.Reflection.AssemblyProductAttribute("SaborCubano.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SaborCubano.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
