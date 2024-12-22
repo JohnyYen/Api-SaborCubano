bd5f82463283a24f59191bd7df906844267acd41
JohnyYen <126342433+JohnyYen@users.noreply.github.com>
2024-12-22 11:30:33 -0500

    prepare-merge

--- SaborCubano.API/obj/Debug/net9.0/SaborCubano.API.AssemblyInfo.cs
+++ SaborCubano.API/obj/Debug/net9.0/SaborCubano.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SaborCubano.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ba938e00bfb0d593676f21a3e3667ed87e3f6a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c294c7139ae78c9e9ffb0bbdf0ecb524a9b2e58")]
 [assembly: System.Reflection.AssemblyProductAttribute("SaborCubano.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SaborCubano.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
